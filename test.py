b508d0f67c7da039c0e989326016f1af117d4de2
Xing <191300064@smail.nju.edu.cn>
2021-02-03 21:55:14 +0800

    结束时间序列分析

--- test.py
+++ test.py
@@ -1,24 +1,5 @@
 # %%
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-data = pd.read_excel('iris.xlsx').drop(axis=1, columns='Unnamed: 0')
-
-# sns.set()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-x, y, z = data['petal_length'], data['sepal_length'], data['sepal_width']
-
-ax.set_xlabel("petal_length")
-ax.set_ylabel("sepal_length")
-ax.set_zlabel("sepal_width")
-
-ax.scatter(x, y, z, c=data.species)
-
-plt.savefig('iris_species_scatter.png')
-
+data = pd.read_excel('ExcelData/iris.xlsx')
 # %%
